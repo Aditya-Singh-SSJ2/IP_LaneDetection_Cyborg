26f5dd0e3f6f0adee28714741eef5ad4f18b7971
AdiTOSH <56103824+Aditya-Singh-SSJ2@users.noreply.github.com>
2021-02-13 11:56:42 +0530

    Add files via upload

---
+++ birds5.0.py
@@ -0,0 +1,383 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt, cm, colors
+
+# Defining variables to hold meter-to-pixel conversion
+ym_per_pix = 30 / 720
+# Standard lane width is 3.7 meters divided by lane width in pixels which is
+# calculated to be approximately 720 pixels not to be confused with frame height
+xm_per_pix = 3.7 / 720
+
+################################################################################
+#### START - APPLY SLIDING WINDOW METHOD TO DETECT CURVES ######################
+def slide_window_search(binary_warped, histogram):
+
+    # Find the start of left and right lane lines using histogram info
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
+    midpoint = np.int(histogram.shape[0] / 2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # A total of 9 windows will be used
+    nwindows = 9
+    window_height = np.int(binary_warped.shape[0] / nwindows)
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    margin = 100
+    minpix = 50
+    left_lane_inds = []
+    right_lane_inds = []
+
+    #### START - Loop to iterate through windows and search for lane lines #####
+    for window in range(nwindows):
+        win_y_low = binary_warped.shape[0] - (window + 1) * window_height
+        win_y_high = binary_warped.shape[0] - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high),
+        (0,255,0), 2)
+        cv2.rectangle(out_img, (win_xright_low,win_y_low), (win_xright_high,win_y_high),
+        (0,255,0), 2)
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+        (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+        (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+    #### END - Loop to iterate through windows and search for lane lines #######
+
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Apply 2nd degree polynomial fit to fit curves
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0])
+    left_fitx = left_fit[0] * ploty**2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty**2 + right_fit[1] * ploty + right_fit[2]
+
+    ltx = np.trunc(left_fitx)
+    rtx = np.trunc(right_fitx)
+    plt.plot(right_fitx)
+    # plt.show()
+
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+    # plt.imshow(out_img)
+    plt.plot(left_fitx,  ploty, color = 'yellow')
+    plt.plot(right_fitx, ploty, color = 'yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+
+    return ploty, left_fit, right_fit, ltx, rtx
+#### END - APPLY SLIDING WINDOW METHOD TO DETECT CURVES ########################
+################################################################################
+
+
+
+################################################################################
+#### START - APPLY GENERAL SEARCH METHOD TO DETECT CURVES ######################
+def general_search(binary_warped, left_fit, right_fit):
+
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    margin = 100
+    left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy +
+    left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) +
+    left_fit[1]*nonzeroy + left_fit[2] + margin)))
+
+    right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy +
+    right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) +
+    right_fit[1]*nonzeroy + right_fit[2] + margin)))
+
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0])
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+
+    ## VISUALIZATION ###########################################################
+
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    window_img = np.zeros_like(out_img)
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin,
+                                  ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+
+    # plt.imshow(result)
+    plt.plot(left_fitx,  ploty, color = 'yellow')
+    plt.plot(right_fitx, ploty, color = 'yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+
+    ret = {}
+    ret['leftx'] = leftx
+    ret['rightx'] = rightx
+    ret['left_fitx'] = left_fitx
+    ret['right_fitx'] = right_fitx
+    ret['ploty'] = ploty
+
+    return ret
+#### END - APPLY GENERAL SEARCH METHOD TO DETECT CURVES ########################
+################################################################################
+
+
+
+################################################################################
+#### START - FUNCTION TO MEASURE CURVE RADIUS ##################################
+def measure_lane_curvature(ploty, leftx, rightx):
+
+    leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
+    rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
+
+    # Choose the maximum y-value, corresponding to the bottom of the image
+    y_eval = np.max(ploty)
+
+    # Fit new polynomials to x, y in world space
+    left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+
+    # Calculate the new radii of curvature
+    left_curverad  = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    # Now our radius of curvature is in meters
+    # print(left_curverad, 'm', right_curverad, 'm')
+
+    # Decide if it is a left or a right curve
+    if leftx[0] - leftx[-1] > 60:
+        curve_direction = 'Left Curve'
+    elif leftx[-1] - leftx[0] > 60:
+        curve_direction = 'Right Curve'
+    else:
+        curve_direction = 'Straight'
+
+    return (left_curverad + right_curverad) / 2.0, curve_direction
+#### END - FUNCTION TO MEASURE CURVE RADIUS ####################################
+################################################################################
+
+
+
+################################################################################
+#### START - FUNCTION TO VISUALLY SHOW DETECTED LANES AREA #####################
+def draw_lane_lines(original_image, warped_image, Minv, draw_info):
+
+    leftx = draw_info['leftx']
+    rightx = draw_info['rightx']
+    left_fitx = draw_info['left_fitx']
+    right_fitx = draw_info['right_fitx']
+    ploty = draw_info['ploty']
+
+    warp_zero = np.zeros_like(warped_image).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    mean_x = np.mean((left_fitx, right_fitx), axis=0)
+    pts_mean = np.array([np.flipud(np.transpose(np.vstack([mean_x, ploty])))])
+
+    cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+    cv2.fillPoly(color_warp, np.int_([pts_mean]), (0, 255, 255))
+
+    newwarp = cv2.warpPerspective(color_warp, Minv, (original_image.shape[1], original_image.shape[0]))
+    result = cv2.addWeighted(original_image, 1, newwarp, 0.3, 0)
+
+    return pts_mean, result
+#### END - FUNCTION TO VISUALLY SHOW DETECTED LANES AREA #######################
+################################################################################
+
+
+#### START - FUNCTION TO CALCULATE DEVIATION FROM LANE CENTER ##################
+################################################################################
+def offCenter(meanPts, inpFrame):
+
+    # Calculating deviation in meters
+    mpts = meanPts[-1][-1][-2].astype(int)
+    pixelDeviation = inpFrame.shape[1] / 2 - abs(mpts)
+    deviation = pixelDeviation * xm_per_pix
+    direction = "left" if deviation < 0 else "right"
+
+    return deviation, direction
+################################################################################
+#### END - FUNCTION TO CALCULATE DEVIATION FROM LANE CENTER ####################
+
+
+
+################################################################################
+#### START - FUNCTION TO ADD INFO TEXT TO FINAL IMAGE ##########################
+def addText(img, radius, direction, deviation, devDirection):
+
+    # Add the radius and center position to the image
+    font = cv2.FONT_HERSHEY_TRIPLEX
+
+    if (direction != 'Straight'):
+        text = 'Radius of Curvature: ' + '{:04.0f}'.format(radius) + 'm'
+        text1 = 'Curve Direction: ' + (direction)
+
+    else:
+        text = 'Radius of Curvature: ' + 'N/A'
+        text1 = 'Curve Direction: ' + (direction)
+
+    cv2.putText(img, text , (50,100), font, 0.8, (0,100, 200), 2, cv2.LINE_AA)
+    cv2.putText(img, text1, (50,150), font, 0.8, (0,100, 200), 2, cv2.LINE_AA)
+
+    # Deviation
+    deviation_text = 'Off Center: ' + str(round(abs(deviation), 3)) + 'm' + ' to the ' + devDirection
+    cv2.putText(img, deviation_text, (50, 200), cv2.FONT_HERSHEY_TRIPLEX, 0.8, (0,100, 200), 2, cv2.LINE_AA)
+
+    return img
+#### END - FUNCTION TO ADD INFO TEXT TO FINAL IMAGE ############################
+################################################################################
+
+
+
+
+cap = cv2.VideoCapture("Bird's eye view/ip.mp4",0)
+success, frame = cap.read()
+
+def nothing(x):
+    pass
+
+cv2.namedWindow("Trackbars")
+
+cv2.createTrackbar("L - H", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 200, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 50, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+
+leftx = []
+rightx = []
+lefty = []
+righty = []
+
+
+
+while True:
+    success, frame = cap.read()
+
+    frame = cv2.resize(frame, (640, 480)) 
+
+    tl = (225,387)
+    bl = (70,472)
+    tr = (400,380)
+    br = (538,472)
+
+    frame2 = frame.copy()
+    cv2.circle(frame2, tl, 5, (0,255,0), -1)
+    cv2.circle(frame2, bl, 5, (0,255,0), -1)
+    cv2.circle(frame2, tr, 5, (0,255,0), -1)
+    cv2.circle(frame2, br, 5, (0,255,0), -1)
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    frame = cv2.resize(frame, (640, 480)) 
+
+    pts1 = np.float32([tl, bl, tr, br]) 
+    pts2 = np.float32([[0, 0], [0, 600], [500, 0], [500, 600]]) 
+    
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)      
+    minverse = cv2.getPerspectiveTransform(pts2, pts1)  # dst->src
+    result2 = cv2.warpPerspective(hsv, matrix, (500,600)) # BIRD'S EYE
+    
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+    
+    lower = np.array([l_h,l_s,l_v])
+    upper = np.array([u_h,u_s,u_v])
+    mask = cv2.inRange(result2, lower, upper)
+    mask = cv2.resize(mask, (640, 480)) 
+    result = cv2.bitwise_and(frame, frame, mask = mask)
+
+    
+    
+    ### HISTOGRAM
+    # if leftx==0 and rightx==0:
+    histogram = np.sum(mask[mask.shape[0]//2:, :], axis=0)
+    midpoint = np.int(histogram.shape[0]/2)
+    left_base = np.argmax(histogram[:midpoint])
+    right_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    
+    ######################################################################
+    ######################################################################
+    ######################################################################
+    ######################################################################
+    ######################################################################
+    ######################################################################
+    try:
+        ploty, left_fit, right_fit, left_fitx, right_fitx = slide_window_search(mask, histogram)
+
+        plt.plot(left_fit)
+        # plt.show()
+
+
+        draw_info = general_search(mask, left_fit, right_fit)
+        # plt.show()
+
+
+        curveRad, curveDir = measure_lane_curvature(ploty, left_fitx, right_fitx)
+
+
+        # Filling the area of detected lanes with green
+        meanPts, result = draw_lane_lines(frame, mask, minverse, draw_info)
+
+
+        deviation, directionDev = offCenter(meanPts, frame)
+
+
+        # Adding text to our final image
+        finalImg = addText(result, curveRad, curveDir, deviation, directionDev)
+
+    except:
+        pass
+
+    # Displaying final image
+    cv2.imshow("Final", finalImg)
+
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
